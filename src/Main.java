a864fa3c8897fe5582c62e4b9974a0717f462383
bipran-rimal <bipran.workspace@gmail.com>
2024-09-23 18:35:11 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double priceOfAPurchase = 69.99;
+        double salesTax = 0.05 * priceOfAPurchase;
+        System.out.println("The price of the purchase is "+ priceOfAPurchase + " and the sales tax is "+ salesTax);
+    }
+}
